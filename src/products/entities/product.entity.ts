747033192f4056e265a3f1ec2f3379e45e1534fb
valeroman <valeroman@gmail.com>
2024-03-28 10:28:26 -0500

    Primer commit bases nest api

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,32 @@
+interface UpdateWithOpcions {
+    name?: string;
+    description?: string;
+    price?: number;
+}
+
+
+export class Product {
+
+    // public id: string;
+    // public name: string;
+    // public description?: string;
+    // public price: number;
+
+    constructor(
+        public id: string,
+        public name: string,
+        public description: string,
+        public price: number,
+    ) {}
+
+    updateWith({
+        name,
+        description,
+        price,
+    }: UpdateWithOpcions) {
+
+        this.name = name ?? this.name;
+        this.description = description ?? this.description;
+        this.price = price ?? this.price;
+    }
+}
